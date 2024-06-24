8e2c3981d278ac36288b227efc0f8e5d60ee55aa
keishimizu26629 <keisukeshimizu.inoworl@gmail.com>
2024-06-24 17:04:18 +0900

    feat: added typescript

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,10 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World from Express!' });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
